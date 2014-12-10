bf1ef994c65a2205a606ea00524362f623f4ddbf
unknown <somrat@CTRLSOFT-04TE06.(none)>
2014-12-10 08:36:34 +0600

    Initial Commit

---
+++ src/com/example/learnactivity/Second_Activity.java
@@ -0,0 +1,14 @@
+package com.example.learnactivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Second_Activity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.second);
+	}
+
+}
